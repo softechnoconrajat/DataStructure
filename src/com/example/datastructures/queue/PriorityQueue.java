e717853a281c7e78c493e7937f4fa764d434a7f3
rajatkumar <rajatkumar@192.168.1.6>
2018-11-11 15:10:03 +0530

    Add Queue

---
+++ src/com/example/datastructures/queue/PriorityQueue.java
@@ -0,0 +1,26 @@
+package com.example.datastructures.queue;
+
+public class PriorityQueue {
+	
+	int data;
+	int priority;
+	
+	
+	
+	public static void insert(int data, int priority) {
+		
+		
+		
+		
+	}
+	
+	public static void main (String[] args) {
+		
+		
+		
+		 
+	}
+	
+	
+	
+}
