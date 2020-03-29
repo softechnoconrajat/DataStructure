dcde75395260b5abda265a365aeaa2bb755cb17c
rajat.kumar <rajat.kumar@smartfren.com>
2020-03-29 23:21:53 +0530

    adding HouseTheifProblem with the dynamic solution

--- src/DnQ /src/com/rajat/problems/HouseTheif.java
+++ src/DnQ /src/com/rajat/problems/HouseTheif.java
@@ -6,10 +6,34 @@ public class HouseTheif {
 
     public int houseTheif(int index, int[] houseArray){
 
+        if(index > houseArray.length - 1){
+            return 0;
+        }
+
+
         int currentHouse = houseArray[index] + houseTheif(index + 2, houseArray);
         int skipCurrentHouse =  houseTheif(index + 1, houseArray);
         return Math.max(currentHouse, skipCurrentHouse);
     }
+
+
+    public int houseTheifDnP(int index, int[] houseArray) {
+
+        if(index > houseArray.length - 1){
+            return 0;
+        }
+
+        int[] arr = new int[houseArray.length];
+
+        if(arr[index] == 0){
+            int currentHouse = houseArray[index] + houseTheifDnP(index + 2, houseArray);
+            int skipCurrentHouse =  houseTheifDnP(index + 1, houseArray);
+            arr[index] = Math.max(currentHouse, skipCurrentHouse);
+        }
+        return arr[index];
+    }
+
+
 
 
 }
--- src/DnQ /src/com/rajat_coding/Main.java
+++ src/DnQ /src/com/rajat_coding/Main.java
@@ -1,8 +1,18 @@
 package com.rajat_coding;
 
+import com.rajat.problems.HouseTheif;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int[] a = {6, 7, 1, 30, 8, 2, 4};
+        int[] b = {20, 5, 1, 13, 6, 11, 40};
+        HouseTheif x = new HouseTheif();
+        System.out.println(x.houseTheif(0, a));
+        System.out.println(x.houseTheif(0, b));
+
+        System.out.println(x.houseTheifDnP(0, a));
+
     }
 }
